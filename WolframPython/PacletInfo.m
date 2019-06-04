07d5566aaa1c160775445aa1126ab95be9732e42
Arnoud Buzing <arnoudb@wolfram.com>
2019-06-04 07:53:30 -0500

    updates

---
+++ WolframPython/PacletInfo.m
@@ -0,0 +1,13 @@
+Paclet[
+  Name -> "WolframPython",
+  Description -> "Wolfram Language functions which use functionality from Python",
+  Creator -> "Arnoud Buzing",
+  Publisher -> "Wolfram Research",
+  Version -> "0.0.1",
+  MathematicaVersion -> "11.2+",
+  Loading -> Automatic,
+  Extensions -> {
+   { "Documentation", Language -> "English"},
+   { "Kernel", Symbols -> {}, Context -> {"WolframPython`"} }
+  }
+]
